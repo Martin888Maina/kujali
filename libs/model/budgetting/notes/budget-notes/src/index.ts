2429fded2d993be87b798e710181e03c39e4f5e7
Martin888Maina <martmain.k@gmail.com>
2025-11-29 17:13:01 +0300

    chore: export public Endpoint for budget-notes module

--- libs/model/budgetting/notes/budget-notes/src/index.ts
+++ libs/model/budgetting/notes/budget-notes/src/index.ts
@@ -1,1 +1,24 @@
-export * from './lib/model-budgetting-notes-budget-notes';
+
+/**
+ * Public Endpoint for budget-notes module
+ * 
+ */
+//command
+export { 
+  AddNoteToBudgetCommand,
+  AddNoteToBudgetResult 
+} from './lib/domain/add-note.command';
+
+// Handler
+export { 
+  AddNoteToBudgetHandler,
+  ICommandHandler,
+  FunctionHandler,
+  HandlerToolkit
+} from './lib/domain/add-note.handler';
+
+// Repository
+export { 
+  IBudgetNotesRepository,
+  BudgetNote 
+} from './lib/domain/budget-notes.repository';
